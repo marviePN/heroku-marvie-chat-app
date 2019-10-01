d481b6b522076120b17b40a740cccce63b922b5d
marviePN <marvie.cauyan@student.passerellesnumeriques.org>
2019-10-01 20:29:29 +0800

    naunsa na tah ani

--- index.js
+++ index.js
@@ -3,77 +3,77 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 9000;
 var path = require('path');
 var clients = [];
 var add = 1;
 
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-function getUsersList(){
+function getUsersList() {
   var usersList = [];
-    for (var i = 0; i < clients.length; i++){
-      usersList[i] = clients[i].n;
-    }
+  for (var i = 0; i < clients.length; i++) {
+    usersList[i] = clients[i].n;
+  }
   return usersList;
 }
 
-function setUserTyping(index){
+function setUserTyping(index) {
   var usersList = [];
-    for (var i = 0; i < clients.length; i++){
-      usersList[i] = clients[i].n; 
-    }
-  usersList[index] = clients[index].n + "💬 is typing " ;
+  for (var i = 0; i < clients.length; i++) {
+    usersList[i] = clients[i].n;
+  }
+  usersList[index] = clients[index].n + "💬 is typing ";
   return usersList;
 }
 
-app.get('/', function(res){
+app.get('/', function (res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   clients.push(socket);
 
-  socket.on('start', function(){
-    socket.emit('nickName', "guest"+add);
-    clients[clients.indexOf(socket)].n = "guest"+add;
+  socket.on('start', function () {
+    socket.emit('nickName', "guest" + add);
+    clients[clients.indexOf(socket)].n = "guest" + add;
     add++;
     io.emit('users list', getUsersList());
   });
 
-  socket.on('send chat message', function(msg){
+  socket.on('send chat message', function (msg) {
     io.emit('chat message', msg);
   });
 
-  socket.on('set nickName', function(nickName){
-    io.emit('info', "New user: " + nickName); 
-    clients[clients.indexOf(socket)].n = nickName; 
-    io.emit('users list', getUsersList()); 
+  socket.on('set nickName', function (nickName) {
+    io.emit('info', "New user: " + nickName);
+    clients[clients.indexOf(socket)].n = nickName;
+    io.emit('users list', getUsersList());
   });
 
-  socket.on('typing', function(){
-    io.emit('typing signal', setUserTyping(clients.indexOf(socket))); 
+  socket.on('typing', function () {
+    io.emit('typing signal', setUserTyping(clients.indexOf(socket)));
   });
 
-  socket.on('not typing', function(){
-    io.emit('typing signal', getUsersList()); 
+  socket.on('not typing', function () {
+    io.emit('typing signal', getUsersList());
   });
 
-  socket.on('disconnect', function() {
-    if( clients[clients.indexOf(socket)].n == null ){
-     
+  socket.on('disconnect', function () {
+    if (clients[clients.indexOf(socket)].n == null) {
+
     }
-    else{
-     
+    else {
+
       io.emit('info', "User " + clients[clients.indexOf(socket)].n + " disconnected.");
     }
-    clients.splice(clients.indexOf(socket),1);
+    clients.splice(clients.indexOf(socket), 1);
     io.emit('users list', getUsersList());
-   });
+  });
 });
 
 
-  http.listen(port, '0.0.0.0', function(){
-    console.log('listening on *:' + port);
+http.listen(port, '0.0.0.0', function () {
+  console.log('listening on *:' + port);
 });
